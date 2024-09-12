af6352835948eb4781f80b36da25859d8aa85df4
MauricioVqz <131129021+MauricioVqz@users.noreply.github.com>
2024-09-11 23:15:03 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ function generarNumeroSecreto() {
 
 function condicionesIniciales() {
     asignarTextoElemento('h1', 'Juego del número secreto');
-    asignarTextoElemento('p', `Indica un número del 1 al $(numeroMaximo)`);
+    asignarTextoElemento('p', `Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
 }
